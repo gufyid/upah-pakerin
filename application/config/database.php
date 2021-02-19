40686723da8b2bece8567d868ce6c2a0b6b4fe25
gufyid <gufyid@gmail.com>
2021-02-19 08:43:12 +0700

    ubah koneksi database ke 172.17.20.40

--- application/config/database.php
+++ application/config/database.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '172.17.20.40';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'amanda';
 $db['default']['database'] = 'upah';
